c9e869b5cf029bd7c8d96916cf650bd88bc5641b
Haris294 <haris294@gmail.com>
2021-01-08 23:41:35 +0530

    Update hangman.py

--- HANGMAN/hangman.py
+++ HANGMAN/hangman.py
@@ -1,7 +1,6 @@
 from string import ascii_lowercase
 from words import get_random_word
 
-
 def get_num_attempts():
     """Get user-inputted number of incorrect attempts for the game."""
     while True:
